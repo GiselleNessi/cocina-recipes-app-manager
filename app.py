3ece44d82789c4d857e9a5af773669a6aec1c575
Giselle Chacon <gisecn@gmail.com>
2020-06-26 16:24:57 +0100

    Added main functionalities in app.py

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+if os.path.exists("env.py"):
+  import env 
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'recipes_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
+
+
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('add_recipe.html', categories=mongo.db.categories.find())
+
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
+
+
+@app.route('/edit_recipe/<recipes_id>')
+def edit_recipe(recipes_id):
+    the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipes_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('edit_recipe.html', recipes=the_recipe,
+                           categories=all_categories)
+
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
