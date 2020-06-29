94d9d4215f546c457497e6d822d9f4ecf32556cf
Giselle Chacon <gisecn@gmail.com>
2020-06-29 18:02:14 +0100

    Testing main functionalities in app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
-from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId 
 if os.path.exists("env.py"):
   import env 
@@ -18,6 +18,19 @@ def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 
+@app.route('/index')
+def index():
+    recipes = list(mongo.db.recipes.find().sort(
+        "views", pymongo.DESCENDING).limit(4))
+    return render_template('index.html', recipes=recipes)
+
+
+@app.route('/view_recipe/<recipes_id>')
+def view_recipe(recipes_id):
+    recipes = mongo.db.recipes.find_one({"_id": ObjectId(recipes_id)})
+    return render_template('view_recipe.html', recipes=recipes)
+
+
 @app.route('/add_recipe')
 def add_recipe():
     return render_template('add_recipe.html', categories=mongo.db.categories.find())
@@ -32,10 +45,13 @@ def insert_recipe():
 
 @app.route('/edit_recipe/<recipes_id>')
 def edit_recipe(recipes_id):
-    the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipes_id)})
-    all_categories =  mongo.db.categories.find()
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipes_id)})
+    all_categories = mongo.db.categories.find()
     return render_template('edit_recipe.html', recipes=the_recipe,
                            categories=all_categories)
+
+
+
 
 
 
